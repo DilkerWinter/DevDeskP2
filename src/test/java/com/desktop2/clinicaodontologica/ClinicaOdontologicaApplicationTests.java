680c679bb5d2e56216e85f3ca64bee29e0f41735
Laboratorio <laboratorio@lab.unigran>
2024-05-02 20:00:11 -0400

    create java spring

---
+++ src/test/java/com/desktop2/clinicaodontologica/ClinicaOdontologicaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desktop2.clinicaodontologica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicaOdontologicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
